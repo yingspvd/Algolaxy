fc8a5e509fb220131e8ee62f1f8ce2f09e76285b
yingspvd <supavadee.ying@mail.kmutt.ac.th>
2021-07-15 01:28:09 +0700

    New : Toolbox

--- frontend/src/components/Tools.vue
+++ frontend/src/components/Tools.vue
@@ -1,76 +1,225 @@
 <template>
-  <div class="flex-shrink-0 p-3">
+  <div class="flex-shrink-0 p-3 overflow-auto">
     <ul class="list-unstyled ps-0">
       <li class="mb-1">
-       
         <button
           class="btn btn-toggle align-items-center rounded collapsed"
           data-bs-toggle="collapse"
-          data-bs-target="#home-collapse"
+          data-bs-target="#terminator-collapse"
           aria-expanded="true"
         >
-          Home
+          Terminator
         </button>
-        <div class="collapse show" id="home-collapse">
+        <div class="collapse show" id="terminator-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Updates</a></li>
-            <li><a href="#" class="link-dark rounded">Reports</a></li>
+            <div class="flowchart">
+              <div class="terminator" style="margin-right:10px">
+                <div class="terminator-text">
+                  START
+                </div>
+              </div>
+              <div class="terminator">
+                <div class="terminator-text">END</div>
+              </div>
+            </div>
           </ul>
         </div>
       </li>
+
       <li class="mb-1">
         <button
           class="btn btn-toggle align-items-center rounded collapsed"
           data-bs-toggle="collapse"
-          data-bs-target="#dashboard-collapse"
-          aria-expanded="false"
+          data-bs-target="#inout-collapse"
+          aria-expanded="true"
         >
-          Dashboard
+          Input / Output
         </button>
-        <div class="collapse" id="dashboard-collapse">
+        <div class="collapse show" id="inout-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Weekly</a></li>
-            <li><a href="#" class="link-dark rounded">Monthly</a></li>
-            <li><a href="#" class="link-dark rounded">Annually</a></li>
+            <div class="parallelogram">
+              <div class="not-skew">
+                <div class="place" style="margin-left: 10px;">
+                  Read
+                </div>
+                <div class="dropdown-box" style="margin-left: 10px">
+                  <div class="triangle-down"></div>
+                </div>
+              </div>
+            </div>
+            <div class="parallelogram" style="margin-bottom:10px">
+              <div class="not-skew">
+                <div class="place" style="margin-left: 10px;">
+                  Print
+                </div>
+                <div class="dropdown-box" style="margin-left: 10px">
+                  <div class="triangle-down"></div>
+                </div>
+              </div>
+            </div>
           </ul>
         </div>
       </li>
+
       <li class="mb-1">
         <button
           class="btn btn-toggle align-items-center rounded collapsed"
           data-bs-toggle="collapse"
-          data-bs-target="#orders-collapse"
-          aria-expanded="false"
+          data-bs-target="#display-collapse"
+          aria-expanded="true"
         >
-          Orders
+          Display
         </button>
-        <div class="collapse" id="orders-collapse">
+        <div class="collapse show" id="display-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
             <li><a href="#" class="link-dark rounded">New</a></li>
-            <li><a href="#" class="link-dark rounded">Processed</a></li>
-            <li><a href="#" class="link-dark rounded">Shipped</a></li>
-            <li><a href="#" class="link-dark rounded">Returned</a></li>
           </ul>
         </div>
       </li>
-      <li class="border-top my-3"></li>
+
       <li class="mb-1">
         <button
           class="btn btn-toggle align-items-center rounded collapsed"
           data-bs-toggle="collapse"
           data-bs-target="#account-collapse"
-          aria-expanded="false"
+          aria-expanded="true"
+          style="font-size: 14px"
         >
-          Account
+          Declare / Assign Variable
         </button>
-        <div class="collapse" id="account-collapse">
+        <div class="collapse show" id="account-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">New...</a></li>
-            <li><a href="#" class="link-dark rounded">Profile</a></li>
-            <li><a href="#" class="link-dark rounded">Settings</a></li>
-            <li><a href="#" class="link-dark rounded">Sign out</a></li>
+            <div class="square-box-orange">
+              Set
+              <div class="square-textbox"></div>
+              =
+              <div class="square-textbox">
+                0
+              </div>
+            </div>
+
+            <div class="square-box-orange">
+              Set
+              <div class="square-textbox"></div>
+              =
+              <div class="square-textbox">
+                " "
+              </div>
+            </div>
+
+            <div class="square-box-orange">
+              Set
+              <div class="square-textbox"></div>
+              =
+              <div class="square-textbox">
+                [ ]
+              </div>
+            </div>
+
+            <div class="square-box-purple">
+              <div class="dropdown-box" style="width: 40px;">
+                <div class="triangle-down"></div>
+              </div>
+              <div
+                class="dropdown-box"
+                style="border-radius: 10px; width: 40px;"
+              >
+                <div
+                  class="triangle-down"
+                  style=" border-top: 6px solid #E4AD6A; "
+                ></div>
+              </div>
+              <div class="square-textbox">0</div>
+            </div>
+
+            <div class="square-box-purple">
+              <div class="dropdown-box" style="width: 40px;">
+                <div class="triangle-down"></div>
+              </div>
+              <div
+                class="dropdown-box"
+                style="border-radius: 10px; width: 40px;"
+              >
+                <div
+                  class="triangle-down"
+                  style=" border-top: 6px solid #E4AD6A; "
+                ></div>
+              </div>
+              <div class="square-textbox">
+                " "
+              </div>
+            </div>
+            <div class="square-box-purple" style="margin-bottom:10px">
+              <div class="dropdown-box" style="width: 40px;">
+                <div class="triangle-down"></div>
+              </div>
+              <div
+                class="dropdown-box"
+                style="border-radius: 10px; width: 40px;"
+              >
+                <div
+                  class="triangle-down"
+                  style=" border-top: 6px solid #E4AD6A; "
+                ></div>
+              </div>
+              <div class="square-textbox">
+                [ ]
+              </div>
+            </div>
+          </ul>
+        </div>
+      </li>
+
+      <li class="mb-1">
+        <button
+          class="btn btn-toggle align-items-center rounded collapsed"
+          data-bs-toggle="collapse"
+          data-bs-target="#condition-collapse"
+          aria-expanded="true"
+        >
+          Condition
+        </button>
+        <div class="collapse show" id="condition-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <div class="diamond-square-box">
+              <div class="diamond-item">
+                <div class="dropdown-box" style="width: 40px;">
+                  <div class="triangle-down"></div>
+                </div>
+                <div
+                  class="dropdown-box"
+                  style="border-radius: 10px; width: 40px;"
+                >
+                  <div
+                    class="triangle-down"
+                    style=" border-top: 6px solid #E4AD6A; "
+                  ></div>
+                </div>
+
+                <div class="dropdown-box"></div>
+              </div>
+            </div>
+          </ul>
+        </div>
+      </li>
+
+      <li class="mb-1">
+        <button
+          class="btn btn-toggle align-items-center rounded collapsed"
+          data-bs-toggle="collapse"
+          data-bs-target="#function-collapse"
+          aria-expanded="true"
+        >
+          Function
+        </button>
+        <div class="collapse show" id="function-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <div class="square-round">
+              <div class="square-textbox" style=" width: 120px; ">
+                " Function "
+              </div>
+            </div>
+            <div class="square-dark-purple"></div>
           </ul>
         </div>
       </li>
@@ -84,7 +233,7 @@ export default {};
 
 <style>
 body {
-  min-height: 100vh;
+  /* min-height: 80vh; */
   min-height: -webkit-fill-available;
 }
 
@@ -103,9 +252,9 @@ main {
 }
 .flex-shrink-0 {
   width: 280px;
-  height: 100vh;
-  border-radius: 30px;
-  margin-top: 50px;
+  height: 86vh;
+  border-radius: 0 30px 30px 0;
+  margin-top: -4px;
   background-color: var(--dark-blue);
 }
 .b-example-divider {
@@ -142,8 +291,7 @@ main {
   background-color: transparent;
   border: 0;
 }
-.btn-toggle:hover,
-.btn-toggle:focus {
+.btn-toggle:hover {
   color: white;
   background-color: var(--pink);
 }
@@ -171,8 +319,7 @@ main {
   text-decoration: none;
   color: white;
 }
-.btn-toggle-nav a:hover,
-.btn-toggle-nav a:focus {
+.btn-toggle-nav a:hover {
   background-color: var(--pink);
 }
 
@@ -185,5 +332,159 @@ main {
 }
 .lh-tight {
   line-height: 1.25;
+}
+
+.btn.focus,
+.btn:focus {
+  outline: 0;
+  box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 0%);
+}
+
+/* Sidebar */
+.flowchart {
+  align-items: flex-start;
+  display: flex;
+  min-width: 100px;
+  margin-top: 10px;
+}
+
+.terminator {
+  align-items: flex-start;
+  background-color: var(--pink);
+  border-radius: 50px;
+  height: 35px;
+  justify-content: flex-end;
+  min-width: 100px;
+  padding: 5.3px 8.2px;
+}
+
+.terminator-text {
+  color: var(--dark-blue);
+  text-align: center;
+  font-weight: 500;
+}
+
+.parallelogram {
+  width: 200px;
+  height: 35px;
+  transform: skew(-20deg);
+  background: #86e2ff;
+  min-width: 180px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.square-box-orange {
+  width: 200px;
+  height: 35px;
+  background: #ffae74;
+  min-width: 180px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 0 10px 0 10px;
+  margin-top: 10px;
+  font-weight: 500;
+  color: var(--dark-blue);
+}
+
+.square-box-purple {
+  width: 200px;
+  height: 35px;
+  background: #c6b8ff;
+  min-width: 180px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 10px;
+  font-weight: 500;
+}
+
+.diamond-square-box {
+  width: 130px;
+  height: 130px;
+  margin: 10px 0 10px 0;
+  background-color: #ff9fc2;
+  transform: rotateX(45deg) rotateZ(45deg);
+}
+
+.diamond-item {
+  transform: rotateX(0deg) rotateZ(-45deg);
+}
+
+.square-round {
+  align-items: flex-start;
+  background-color: #6181f3;
+  border-radius: 50px;
+  height: 35px;
+  width: 200px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-weight: 500;
+}
+
+.square-dark-purple {
+  align-items: flex-start;
+  background-color: #6181f3;
+  height: 35px;
+  width: 200px;
+  margin-top: 10px;
+  justify-content: flex-end;
+}
+
+.place {
+  height: 12px;
+  letter-spacing: 0;
+  min-width: 41px;
+  font-weight: 500;
+  width: 41;
+  color: var(--dark-blue);
+}
+
+.not-skew {
+  transform: skew(20deg);
+  display: flex;
+}
+
+.dropdown-box {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 60px;
+  height: 20px;
+  background: #ffffff;
+  border-radius: 3px;
+  color: var(--dark-blue);
+}
+
+.triangle-down {
+  width: 0;
+  height: 0;
+  margin-right: 5px;
+  border-left: 3px solid transparent;
+  border-right: 3px solid transparent;
+  border-top: 6px solid #584f84;
+}
+
+.btn-toggle-nav {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.square-textbox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 20px;
+  background: #ffffff;
+  border-radius: 3px;
+  color: var(--dark-blue);
 }
 </style>
