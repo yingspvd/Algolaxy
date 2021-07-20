b78bd1a6d4f79518b9b8c500aaacad6c001da759
yingspvd <supavadee.ying@mail.kmutt.ac.th>
2021-07-20 23:12:54 +0700

    New : Component

---
+++ frontend/src/components/Animation.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="container" style="box-shadow: 0px 5px 10px #91A0A5;">
+    <div class="tool-box">
+      <div class="tool-bottom">
+        <button>back</button>
+        <button>stop</button>
+        <button>play</button>
+        <button>pause</button>
+        <button>next</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#container {
+  height: 600px;
+  width: 500px;
+  /* margin-left: 20px; */
+  border-radius: 5px;
+  background-color: var(--white-gray);
+}
+.tool-box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+.tool-bottom {
+  background-color: var(--dark-blue-1);
+  display: flex;
+  justify-content: space-around;
+}
+</style>
